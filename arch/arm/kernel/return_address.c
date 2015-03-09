746f3444233a1d4eae9cd3e3ad3ca8d868256efd
Joe Wooster <jeebobs@btinternet.com>
2015-03-09 12:12:42 +0000

    Revert "fix unwind warning"
    This reverts commit 27f69e8db558d92564dfe646cf545a35d3a7999c.

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -59,7 +59,7 @@ void *return_address(unsigned int level)
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
 #if defined(CONFIG_ARM_UNWIND)
-#include <asm/unwind.h>
+#warning "TODO: return_address should use unwind tables"
 #endif
 
 void *return_address(unsigned int level)
