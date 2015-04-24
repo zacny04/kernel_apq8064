a9b11fc2d15bf6b6eeca90e6a4e66199dcaf86e4
Vladimir Mitreski <vlatko.mitreski@gmail.com>
2015-04-24 02:24:57 +0200

    cleanup repo
    Change-Id: Ie38481dab791bc56bd549535b100a99482ab14af

--- drivers/gpu/ion/msm/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/gpu/ion/msm/built-in.o :=  /home/airlessproject/RR/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-ld -EL    -r -o drivers/gpu/ion/msm/built-in.o drivers/gpu/ion/msm/msm_ion.o drivers/gpu/ion/msm/ion_cp_common.o 
