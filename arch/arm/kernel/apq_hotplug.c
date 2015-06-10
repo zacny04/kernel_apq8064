91cad0a6072a09485c9d02bc1007a3b73b6d29e0
Tommy-Geenexus <roboter972@gmail.com>
2015-06-10 12:22:16 +0200

    apq_hotplug: rework the driver
    <6>[    2.931365] apq_hotplug: initialized!
    <6>[    2.931610] apq_hotplug: CPUs running: 4
    <6>[  128.535491] apq_hotplug: CPU1 down.
    <6>[  128.541595] apq_hotplug: CPU2 down.
    <6>[  128.556396] apq_hotplug: CPU3 down.
    <6>[  129.152954] apq_hotplug: CPU1 up.
    <6>[  129.154998] apq_hotplug: CPU2 up.
    <6>[  129.157104] apq_hotplug: CPU3 up.
    
    Now the driver works as intended, finally.

--- arch/arm/kernel/apq_hotplug.c
+++ arch/arm/kernel/apq_hotplug.c
@@ -1,8 +1,8 @@
 /*
- * apq_hotplug - a multicore hotplug driver
+ * apq_hotplug - multicore hotplug driver
  *
  * Copyright (C) 2015 Tom G. <roboter972@gmail.com>
- *     
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -12,81 +12,140 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * Note: When compiling this driver, you need to add missing __cpuinit's
+ * or remove all __cpuinit's from your kernel source.
+ *
+ * Major Changes:
+ * 20.03.15: Initial driver release
+ * 12.06.15: Complete re-write
  */
 
 #define pr_fmt(fmt) "apq_hotplug: " fmt
 
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
+#include <linux/earlysuspend.h>
 #include <linux/init.h>
-#include <linux/pm.h>
-#include <linux/suspend.h>
+#include <linux/jiffies.h>
+#include <linux/printk.h>
+#include <linux/workqueue.h>
 
-static struct notifier_block pm_notifier_block;
+/* Note: do not release debug builds, as this will flood the log. */
+#define DEBUG
+#undef DEBUG
 
-static inline void offline_all(void)
+static struct workqueue_struct *apq_hotplug_wq;
+static struct delayed_work offline_all_work;
+static struct delayed_work online_all_work;
+
+static unsigned int boot_flag = 0;
+static unsigned int suspend_delay = 100;
+static unsigned int resume_delay = 10;
+
+static inline void offline_all_fn(struct work_struct *work)
 {
 	unsigned int cpu;
 
-	for_each_possible_cpu(cpu) {
-		if (cpu_online(cpu) && (cpu))
+	for_each_online_cpu(cpu) {
+		if (cpu != 0) {
 			cpu_down(cpu);
+#ifdef DEBUG
+			pr_info("CPU%u down.\n", cpu);
+#endif
+		}
 	}
 }
 
-static inline void online_all(void)
+static inline void online_all_fn(struct work_struct *work)
 {
 	unsigned int cpu;
 
-	for_each_possible_cpu(cpu) {
-		if (!cpu_online(cpu))
-			cpu_up(cpu);
+	for_each_cpu_not(cpu, cpu_online_mask) {
+		if (cpu == 0)
+			continue;
+		cpu_up(cpu);
+#ifdef DEBUG
+		pr_info("CPU%u up.\n", cpu);
+#endif
 	}
 }
 
-static void __apq_hotplug_suspend(void)
+static void apq_hotplug_early_suspend(struct early_suspend *h)
 {
-	offline_all();
-}
-
-static void __apq_hotplug_resume(void)
-{
-	online_all();
-}
-
-int apq_hotplug_pm_notify(struct notifier_block *notify_block,
-					unsigned long mode, void *unused)
-{
-	switch (mode) {
-	case PM_HIBERNATION_PREPARE:
-	case PM_SUSPEND_PREPARE:
-		__apq_hotplug_suspend();
-		break;
-	case PM_POST_SUSPEND:
-	case PM_POST_HIBERNATION:
-	case PM_POST_RESTORE:
-		__apq_hotplug_resume();
-		break;
+	/*
+	 * Init new work on the first suspend call,
+	 * skip clearing workqueue as no work has been inited yet.
+	 */
+	if (!boot_flag) {
+		cancel_delayed_work_sync(&online_all_work);
+		flush_workqueue(apq_hotplug_wq);
 	}
 
-	return NOTIFY_OK;
+	INIT_DELAYED_WORK(&offline_all_work, offline_all_fn);
+
+	/*
+	 * Set the boot_flag to zero to allow the clearing of old work
+	 * after the first suspend call.
+	 */
+	if (boot_flag) {
+		--boot_flag;
+	}
+
+	queue_delayed_work(apq_hotplug_wq, &offline_all_work,
+					msecs_to_jiffies(suspend_delay));
 }
+
+static void apq_hotplug_late_resume(struct early_suspend *h)
+{
+	/* Clear the workqueue */
+	cancel_delayed_work_sync(&offline_all_work);
+	flush_workqueue(apq_hotplug_wq);
+	INIT_DELAYED_WORK(&online_all_work, online_all_fn);
+
+	queue_delayed_work(apq_hotplug_wq, &online_all_work,
+					msecs_to_jiffies(resume_delay));
+}
+
+static struct early_suspend __refdata apq_hotplug_early_suspend_handler = {
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+	.suspend = apq_hotplug_early_suspend,
+	.resume = apq_hotplug_late_resume,
+};
 
 static int __init apq_hotplug_init(void)
 {
-	int ret = 0;
+	apq_hotplug_wq = alloc_workqueue("apq_hotplug_wq",
+					WQ_HIGHPRI | WQ_UNBOUND, 1);
+	if (!apq_hotplug_wq) {
+		pr_err("Failed to allocate apq_hotplug workqueue!\n");
+		return -ENOMEM;
+	}
 
-	pm_notifier_block.notifier_call = apq_hotplug_pm_notify;
-	register_pm_notifier(&pm_notifier_block);
+	register_early_suspend(&apq_hotplug_early_suspend_handler);
+
+	/*
+	 * Increment boot_flag to allow skipping of clearing work on
+	 * the first suspend call.
+	 */
+	++boot_flag;
 
 	pr_info("initialized!\n");
 
-	return ret;
+#ifdef DEBUG
+	pr_info("CPUs running: %u\n", num_online_cpus());
+#endif
+
+	return 0;
 }
 
 static void __exit apq_hotplug_exit(void)
 {
-	unregister_pm_notifier(&pm_notifier_block);
+	cancel_delayed_work_sync(&offline_all_work);
+	cancel_delayed_work_sync(&online_all_work);
+	flush_workqueue(apq_hotplug_wq);
+	destroy_workqueue(apq_hotplug_wq);
+
+	unregister_early_suspend(&apq_hotplug_early_suspend_handler);
 }
 
 late_initcall(apq_hotplug_init);
