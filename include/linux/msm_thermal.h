04035a9a56ca2f5bed4faf1f3e758ed02afc988a
BrateloSlava <d_slava@list.ru>
2015-12-26 13:12:50 +0200

    drivers:thermal: port @Tommy-Geenexus patches

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, Tom G. <roboter972@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,12 +18,19 @@
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	uint32_t limit_temp_degC;
-	uint32_t temp_hysteresis_degC;
-	uint32_t freq_step;
+	uint32_t throttle_poll_ms;
+	uint32_t shutdown_temp;
+
+	uint32_t allowed_high_temp;
+	uint32_t allowed_high_rel_temp;
+	uint32_t allowed_high_freq;
+
+	uint32_t allowed_low_temp;
+	uint32_t allowed_low_rel_temp;
+	uint32_t allowed_low_freq;
 };
 
-#ifdef CONFIG_THERMAL_MONITOR
+#ifdef CONFIG_THERMAL_MONITOR		
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 #else
