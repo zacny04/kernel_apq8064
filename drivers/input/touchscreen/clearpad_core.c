5df050d73a34a1b1883e7d8c5e089699e20b0157
BrateloSlava <d_slava@list.ru>
2016-01-13 14:28:30 +0200

    clearpad: optimize locking in clearpad_hard_handler()
    Idea is taken here - https://github.com/AndroPlus-org/android_kernel_sony_msm8994_kitakami_r2/commit/14d5a3249c6281cbc8008e2069d269af947e1526

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -2030,16 +2030,19 @@ static irqreturn_t synaptics_clearpad_hard_handler(int irq, void *dev_id)
 	unsigned long flags;
 	irqreturn_t ret;
 
-	spin_lock_irqsave(&this->slock, flags);
 	if (unlikely(this->dev_busy)) {
+		spin_lock_irqsave(&this->slock, flags);
 		this->irq_pending = true;
+		spin_unlock_irqrestore(&this->slock, flags);
 		dev_info(&this->pdev->dev, "Wake on touch\n");
 		ret = IRQ_HANDLED;
 	} else {
+		spin_lock_irqsave(&this->slock, flags);
 		this->dev_busy = true;
+		spin_unlock_irqrestore(&this->slock, flags);
 		ret = IRQ_WAKE_THREAD;
 	}
-	spin_unlock_irqrestore(&this->slock, flags);
+
 	return ret;
 }
 
