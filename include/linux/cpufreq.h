47dff70473f655aac9a9e6d2f914d77d48b70a58
BrateloSlava <d_slava@list.ru>
2016-02-10 10:31:41 +0200

    BOOT_CPU=0 for alucard

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -365,7 +365,10 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 }
 #endif
 
-
+enum {
+	BOOT_CPU = 0,
+};
+  
 #define MAX_FREQ_LIMIT 1512000
 
 /*********************************************************************
