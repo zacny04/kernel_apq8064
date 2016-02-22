45469e3711179dae7fc57ef65e75db3d0df1fd45
BrateloSlava <d_slava@list.ru>
2016-02-22 02:02:04 +0100

    Revert "kgsl_pwrscale: Rework policy sysfs implementation"
    This reverts commit e0c96a860803eb3af83464992e304434831911a2.

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -269,6 +269,9 @@ int kgsl_pwrscale_policy_add_files(struct kgsl_device *device,
 {
 	int ret;
 
+	kobject_del(&pwrscale->kobj);
+	kobject_put(&pwrscale->kobj);
+
 	ret = kobject_add(&pwrscale->kobj, &device->pwrscale_kobj,
 		"%s", pwrscale->policy->name);
 
@@ -367,11 +370,7 @@ int kgsl_pwrscale_init(struct kgsl_device *device)
 	if (ret)
 		return ret;
 
-        ret = kobject_init_and_add(&device->pwrscale.kobj, &ktype_pwrscale_policy,
-		&device->pwrscale_kobj, "policy_config");
-	if (ret)
-		return ret;
-
+	kobject_init(&device->pwrscale.kobj, &ktype_pwrscale_policy);
 	return ret;
 }
 EXPORT_SYMBOL(kgsl_pwrscale_init);
