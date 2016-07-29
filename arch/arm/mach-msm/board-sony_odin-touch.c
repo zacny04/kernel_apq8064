848dce5a9f347e06489ec3cbab866ae0d89579f2
airlessproject <vlatko.mitreski@gmail.com>
2016-07-29 20:08:11 +0200

    Partially Revert "Revert "dogo, odin, pollux: enable wake gestures""
    This partially reverts commit a5514e391fa38d0e165528a2ab792e4bba77dbc2.

--- arch/arm/mach-msm/board-sony_odin-touch.c
+++ arch/arm/mach-msm/board-sony_odin-touch.c
@@ -33,34 +33,58 @@ int clearpad_flip_config_get(u8 module_id, u8 rev)
 }
 
 static struct evgen_record double_tap[] = {
-	{
-		.type = EVGEN_LOG,
-		.data.log.message = "=== DOUBLE TAP ===",
-	},
-	{
-		.type = EVGEN_KEY,
-		.data.key.code = KEY_POWER,
-		.data.key.down = true,
-	},
-	{
-		.type = EVGEN_KEY,
-		.data.key.code = KEY_POWER,
-		.data.key.down = false,
-	},
-	{
-		.type = EVGEN_END,
-	},
+       {
+               .type = EVGEN_LOG,
+               .data.log.message = "=== DOUBLE TAP ===",
+       },
+       {
+               .type = EVGEN_KEY,
+               .data.key.code = KEY_POWER,
+               .data.key.down = true,
+       },
+       {
+               .type = EVGEN_KEY,
+               .data.key.code = KEY_POWER,
+               .data.key.down = false,
+       },
+       {
+               .type = EVGEN_END,
+       },
+};
+
+static struct evgen_record two_swipe[] = {
+       {
+               .type = EVGEN_LOG,
+               .data.log.message = "=== Two Swipe ===",
+       },
+       {
+               .type = EVGEN_KEY,
+               .data.key.code = KEY_POWER,
+               .data.key.down = true,
+       },
+       {
+               .type = EVGEN_KEY,
+               .data.key.code = KEY_POWER,
+               .data.key.down = false,
+       },
+       {
+               .type = EVGEN_END,
+       },
 };
 
 static struct evgen_block evgen_blocks[] = {
-	{
-		.name = "double_tap",
-		.records = double_tap,
-	},
-	{
-		.name = NULL,
-		.records = NULL,
-	}
+       {
+               .name = "double_tap",
+               .records = double_tap,
+       },
+       {
+               .name = "two_swipe",
+               .records = two_swipe,
+       },
+       {
+               .name = NULL,
+               .records = NULL,
+       }
 };
 
 struct evgen_block *clearpad_evgen_block_get(u8 module_id, u8 rev)
